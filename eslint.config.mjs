b7e0e5e22984bcf8d7784fa20e2b61437cc7dbad
IdleSaur <idlesaurgame@gmail.com>
2025-08-13 08:56:52 -0400

    Disable storybook telemetry, turn off linting on src/generated, using regular generated Post model

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,4 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
+import storybook from 'eslint-plugin-storybook';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -13,8 +11,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+    {
+        ignores: ['src/generated/**'],
+    },
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
-    ...storybook.configs["flat/recommended"]
+    ...storybook.configs['flat/recommended'],
 ];
 
 export default eslintConfig;
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { Prisma } from '@prisma/client';
+import { Post } from '@/generated/prisma';
 import { Routes } from '@/constants';
 import { LinkButton, Heading } from '@/components/ui';
 
 export interface PostPreviewProps {
-    post: Prisma.PostUncheckedCreateInput;
+    post: Post;
 }
 
 export const PostPreview = ({ post }: PostPreviewProps) => {
     return (
-        <div className="flex flex-col items-center justify-center gap-3 pt-5">
-            <Heading level={2}>{post.title}</Heading>
+        <div className="flex flex-col items-center justify-center gap-3">
+            <Heading level={4}>{post.title}</Heading>
             <LinkButton href={Routes.POST.replace(':id', String(post.id))}>
                 {'>'}
             </LinkButton>
@@ -20,7 +20,7 @@ export const PostPreview = ({ post }: PostPreviewProps) => {
 };
 
 export interface HomePageComponentProps {
-    posts: Prisma.PostUncheckedCreateInput[];
+    posts: Post[];
 }
 
 export const Home = ({ posts }: HomePageComponentProps) => {
@@ -28,10 +28,13 @@ export const Home = ({ posts }: HomePageComponentProps) => {
         <div className="flex min-h-screen flex-col items-center justify-center gap-3">
             <Heading>Idle Game</Heading>
             <LinkButton href={Routes.GAME}>Play</LinkButton>
-            <div className="flex flex-row items-center justify-start gap-3">
-                {posts.map((post: Prisma.PostUncheckedCreateInput) => (
-                    <PostPreview post={post} key={post.id} />
-                ))}
+            <div className="mt-40 flex flex-col items-center justify-center gap-3">
+                <Heading level={2}>News</Heading>
+                <div className="flex flex-row items-center justify-start gap-3">
+                    {posts.map((post: Post) => (
+                        <PostPreview post={post} key={post.id} />
+                    ))}
+                </div>
             </div>
         </div>
     );
