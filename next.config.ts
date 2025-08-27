495eccd8fdf1fb53513b2f141357ee9a7c6e3e4a
Tim <5558561+timmendenhall@users.noreply.github.com>
2025-08-27 10:30:09 -0400

    Adding google profile image to allowed images remote patterns

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+            { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
         ],
     },
 };
