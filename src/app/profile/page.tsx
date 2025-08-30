d3922693e2c5bdaad271e1498acd45d00a7774aa
Tim <5558561+timmendenhall@users.noreply.github.com>
2025-08-30 09:26:48 -0400

    Placeholder page for profile / testing neon db branching

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+export type Props = object;
+
+export default async function Page({}: Props) {
+    return (
+        <div>
+            <main>
+                <h1>Profile</h1>
+            </main>
+        </div>
+    );
+}
