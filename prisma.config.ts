388f3a050ab78c6774d8065fd8b1c4cbe3a6917f
Idlesaur <idlesaurgame@gmail.com>
2025-09-10 12:59:48 -0400

    Updated packages and split prisma schema to multiple files (#46)
    Co-authored-by: Tim <5558561+timmendenhall@users.noreply.github.com>

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
 import { PrismaConfig } from 'prisma';
+import path from 'node:path';
 
 export default {
     migrations: {
         seed: `tsx prisma/seed.ts`,
     },
+    schema: path.join('prisma'),
 } satisfies PrismaConfig;
