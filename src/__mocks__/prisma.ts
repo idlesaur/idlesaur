516018566ced56cf8abf842b485310f02ace0388
Tim <5558561+timmendenhall@users.noreply.github.com>
2025-09-16 14:51:22 -0400

    Remove unused mock

--- src/__mocks__/prisma.ts
+++
@@ -1,8 +0,0 @@
-export const prisma = {
-    user: {
-        findMany: async () => [{ id: '1', name: 'Mock User' }],
-    },
-    dinosaur: {
-        findMany: async () => [{ id: 'd1', name: 'Mock Rex' }],
-    },
-};
